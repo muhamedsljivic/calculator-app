1b7c617fd973b9020cf2376d63cdaed9825e8ee3
Muhamed Sljivic <muhamedica09@gmail.com>
2022-11-07 03:41:23 +0100

    dply

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const operators = ["/", "*", "/", "-", "+"];
+
+  const [num, setNum] = useState("");
+  const [result, setResult] = useState(0);
+
+  const updateNum = (value) => {
+    if (
+      (operators.includes(value) && num === "") ||
+      (operators.includes(value) && operators.includes(num.slice(-1)))
+    ) {
+      return;
+    }
+    setNum(num + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(num + value).toString());
+    }
+  };
+
+  const inputHandler = (e) => {
+    console.log(e);
+  };
+
+  const acHandler = () => {
+    setResult(0);
+    setNum("");
+  };
+
+  const solutionHandler = () => {
+    setNum("");
+  };
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateNum(i.toString())} key={i} className="btn">
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="div">
+        <input
+          className="input"
+          onChange={inputHandler}
+          value={result}
+          type="number"
+          name="someid"
+        />
+        <label className="label">{num}</label>
+        <div className="grid">
+          <div className=".btn--div operators">
+            <button onClick={() => updateNum("+")} className="btn--o">
+              +
+            </button>
+            <button onClick={() => updateNum("*")} className="btn--o">
+              *
+            </button>
+            <button onClick={() => updateNum("-")} className="btn--o">
+              -
+            </button>
+            <button onClick={() => updateNum("/")} className="btn--o">
+              /
+            </button>
+            <button onClick={acHandler} className="btn--o">
+              AC
+            </button>
+          </div>
+          <div className=".btn--div digits">
+            {createDigits()}
+            <button
+              onClick={(e) => {
+                updateNum(e.target.innerHTML.toString());
+              }}
+              className="btn"
+            >
+              0
+            </button>
+            <button onClick={() => updateNum(".")} className="btn">
+              .
+            </button>
+            <button onClick={solutionHandler} className="btn">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
